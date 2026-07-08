31bb5d4c809ae6be92eb5308f2d12e204a2f7f20
fivesaw <goatedsaw@gmail.com>
2026-07-08 23:41:10 +0530

    Configure Veil Chat Firebase

---
+++ veil-chat/firebase-config.js
@@ -0,0 +1,11 @@
+// Copy this file to firebase-config.js, then paste your Firebase Web App config.
+// This config is public by design. DO NOT put service-account private keys here.
+window.VEIL_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBqznJ8urwXwgUZ-KJR7dLW6UYMU2cgxK4",
+  authDomain: "veil5chat.firebaseapp.com",
+  projectId: "veil5chat",
+  storageBucket: "veil5chat.firebasestorage.app",
+  messagingSenderId: "1010479977878",
+  appId: "1:1010479977878:web:f7d81b8ab768213cfab9b7",
+  measurementId: "G-HVSPF2821K"
+};
